0fec1013757b4fd43a700f911d59617654edd8d1
HarshMoradiya <harshmoradiya111@gmail.com>
2026-04-15 00:28:41 +0530

    chore: update web-next/src/config/index.ts

--- web-next/src/config/index.ts
+++ web-next/src/config/index.ts
@@ -1,3 +1,3 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
 export const UPLOADS_URL = `${API_URL}/uploads`;
 export const IMAGES_URL = `${API_URL}/img`;
